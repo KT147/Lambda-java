b154b69a5b52e5e27bd65ace89e1caae8d6640de
KT147 <kevin.tammm@gmail.com>
2026-04-29 14:08:29 +0300

    Initial commit

---
+++ src/lambdaExpression/Main.java
@@ -0,0 +1,41 @@
+package lambdaExpression;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>(List.of(
+                "alpha", "bravo", "charlie"
+        ));
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+        System.out.println("----");
+        list.forEach(s -> System.out.println(s));
+
+        System.out.println("----");
+        String prefix = "nato";
+        list.forEach((var myString) -> {
+            char first = myString.charAt(0);
+            System.out.println(prefix + " " + myString + " means " + first);
+        });
+
+        int result = calculator((a, b) -> {
+            return a + b;
+        }, 5, 2);
+        var result2 = calculator((a, b) -> a / b, 10.0, 2.5);
+        var result3 = calculator((a, b) -> a.toUpperCase() + " " + b.toUpperCase(), "Ralph", "Kramden");
+
+    }
+
+    public static <T> T calculator(Operation<T> function, T value1, T value2) {
+        T result = function.operate(value1, value2);
+
+        System.out.println("Result of operation: " + result);
+        return result;
+    }
+}
---
+++ src/lambdaExpression/Operation.java
@@ -0,0 +1,7 @@
+package lambdaExpression;
+
+@FunctionalInterface
+public interface Operation<T> {
+
+    T operate(T value1, T value2);
+}
